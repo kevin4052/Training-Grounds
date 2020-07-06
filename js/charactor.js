b1a76817540462b914d55ca4ad8f68a3ce6c7d3f
Kevin Hernandez <cobra40@gmail.com>
2020-07-06 15:43:14 -0400

    cleaned up the character class

--- js/charactor.js
+++ js/charactor.js
@@ -10,10 +10,10 @@ class Character {
         this.y = y;        
         this.xVel = 0;
         this.yVel = 0;
-        this.moveSpeed = 5;
-        this.gravity = 0.5;
+        this.moveSpeed = 15;
+        this.gravity = 2;
         this.friction = 0.85;
-        this.jump = true;
+        this.jump = {'state': true, 'count': 0};
         this.holdRight = false;
         this.holdLeft = false;
         this.onGround = false;
@@ -32,21 +32,32 @@ class Character {
         if (this.holdRight){
             this.xVel = this.moveSpeed;
         }
+        // if (!this.jump.state){
+        //     this.yVel = -this.moveSpeed * 2;
+        // }
 
         this.x += this.xVel;
         this.y += this.yVel;
         
         if (this.y > this.canvas.height - this.height){
             this.xVel *= this.friction;
-            this.jump = true;
+            this.jump.state = true;
         } else {
             this.yVel += this.gravity;
         }
 
         if (this.y > this.canvas.height - this.height){
-            this.jump = false;
+            this.jump.state = false;
             this.y = this.canvas.height - this.height;
             this.yVel = 0;
+        }
+
+        if (this.x - this.width > this.canvas.width){
+            this.x = 0;
+        }
+
+        if (this.x < -this.width) {
+            this.x = this.canvas.width - this.width;
         }
     }
 
@@ -56,45 +67,14 @@ class Character {
             this.holdLeft = true;
             break;
             case 38:
-                if(this.jump === false) this.yVel = -this.moveSpeed * 2;
+                if(this.jump.state === false) {
+                   this.yVel = -this.moveSpeed * 2;
+                }
                 break;
             case 39:
                 this.holdRight = true;
                 break;
         }
-        // if (event === 39 && this.x < this.canvas.width - this.width) {
-        //     this.xVel += this.moveSpeed;
-        // }
-        // if (event === 37 && this.x > 0) {
-        //     this.xVel -= this.moveSpeed;
-        // }
-        // if (event === 38 && this.jump === false) {
-        //     this.yVel -= 70;
-        //     this.jump = true;
-        // }
-
-        // this.yVel += this.gravity;
-        // this.x += this.xVel;
-        // this.y += this.yVel;
-        // this.xVel *= this.friction;
-        // this.yVel *= this.friction;
-
-        // if (this.y > this.canvas.height - this.height){
-        //     this.jump = false;
-        //     this.y = this.canvas.height - this.height;
-        //     this.yVel = 0;
-        // }
-
-        // if (this.jump) this.yVel += this.gravity + 4;
-
-        // if (this.x - this.width > this.canvas.width){
-        //     this.x = 0;
-        // }
-
-        // if (this.x < -this.width) {
-        //     this.x = this.canvas.width - this.width;
-        // }
-        // console.log('update');
     }
 
     keyUp(event){
